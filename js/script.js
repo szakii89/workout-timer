05237883af580869a7766b87cfccbe597b222b0f
szakii89 <szakii89@yahoo.com>
2023-02-07 06:34:39 +0200

    added settings to ui

--- js/script.js
+++ js/script.js
@@ -1,26 +1,5 @@
-const warmUpLength = 3;
-const totalRounds = 45;
-const roundLength = 5;
-const totalLength = totalRounds * roundLength;
-
-// sounds
-const countAllSeconds = true;
-const countDown = 0;
-const countUp = 0;
-
-const countAllSecondSound = 'metronome-01';
-const countDownSound = 'metronome-07';
-// const countDownSound = 'metronome-01';
-const countUpSound = 'metronome-03';
-const roundStartSound = 'bell-01';
-
-const audioCountAll = new Audio(`..\\audio\\${countAllSecondSound}.mp3`);
-const audioCountDown = new Audio(`..\\audio\\${countDownSound}.mp3`);
-const audioCountUp = new Audio(`..\\audio\\${countUpSound}.mp3`);
-const audioRoundStart = new Audio(`..\\audio\\${roundStartSound}.mp3`);
-
-// Elements
-const currentRoundDetails = document.querySelector('.current-round');
+// ---------- Elements ----------
+const currentRoundDetails = document.querySelector('.current-round-details');
 const elapsedDetails = document.querySelector('.elapsed-details');
 const intervalsDetails = document.querySelector('.intervals-details');
 const remainingDetails = document.querySelector('.remaining-details');
@@ -30,13 +9,68 @@ const nextBtn = document.querySelector('.next-btn');
 const prevBtn = document.querySelector('.previous-btn');
 const resetBtn = document.querySelector('.reset-btn');
 const displayRounds = document.querySelector('.display-rounds');
+const settings = document.querySelector('.settings');
+const warmUplengthEl = document.querySelector('#warm-up-length');
+const totalRoundsEl = document.querySelector('#total-rounds');
+const roundLengthEl = document.querySelector('#round-length');
+
+// ---------- Audio ----------
+const countAllSeconds = false;
+const countDown = 20;
+const countUp = 0;
+
+const countAllSecondSound = 'metronome-19';
+// const countAllSecondSound = 'blip';
+// const countDownSound = 'metronome-07';
+const countDownSound = 'metronome-01';
+const countUpSound = 'metronome-03';
+const roundStartSound = 'bell-01';
+
+const audioCountAll = new Audio(`..\\audio\\${countAllSecondSound}.mp3`);
+const audioCountDown = new Audio(`..\\audio\\${countDownSound}.mp3`);
+const audioCountUp = new Audio(`..\\audio\\${countUpSound}.mp3`);
+const audioRoundStart = new Audio(`..\\audio\\${roundStartSound}.mp3`);
+
+// Play audio whet start/pause is clicked
+const pause = 'ping-01';
+const unpause = 'ping-01';
+const audioPause = new Audio(`..\\audio\\${pause}.mp3`);
+const audioUnpause = new Audio(`..\\audio\\${unpause}.mp3`);
+
+// ---------- Variables ----------
+let warmUpLength = 30;
+let totalRounds = 25;
+let roundLength = 120;
+let totalLength = totalRounds * roundLength;
+
+let currentRound = roundLength;
+let totalRemaining = totalLength;
+let currentInterval = 1;
+let warmUpPassed = 0;
+let elapsedSeconds = 0;
+let t;
+
+// ---------- Functions ----------
+const setWorkout = function (e) {
+  e.preventDefault();
+  warmUpLength = warmUplengthEl.value * 1;
+  totalRounds = totalRoundsEl.value * 1;
+  roundLength = roundLengthEl.value * 1;
+  totalLength = totalRounds * roundLength;
+  currentRound = roundLength;
+  totalRemaining = totalLength;
+  currentInterval = 1;
+  warmUpPassed = 0;
+  displayRoundsFunction();
+  init(totalRounds, roundLength, totalLength);
+};
 
 // Transform seconds
-// IDEA - transformSeconds(seconds,'secs') to display ss
-// IDEA - transformSeconds(seconds,'mins') to display mm:ss
-// IDEA - transformSeconds(seconds,'hrs') to display hh:mm:ss
-
 const transformSeconds = (seconds, displaySettings = 'auto') => {
+  // IDEA - transformSeconds(seconds,'secs') to display ss
+  // IDEA - transformSeconds(seconds,'mins') to display mm:ss
+  // IDEA - transformSeconds(seconds,'hrs') to display hh:mm:ss
+
   let hours = Math.floor(seconds / 3600);
   let minutes = Math.floor((seconds % 3600) / 60);
   let remainingSeconds = seconds % 60;
@@ -72,10 +106,12 @@ const displayRoundsFunction = () => {
     boxes = 1;
     html = '<div class="box box--warm-up box-0"></div>';
   }
+  let startCounting = boxes;
   boxes += totalRounds;
-  for (let i = 1; i < boxes; i++) {
+  for (let i = startCounting; i < boxes; i++) {
     html += `<div class="box box--round box-${i}"></div>`;
   }
+
   displayRounds.innerHTML = html;
   displayRounds.style.gridTemplateColumns = `repeat(${boxes}, 1fr)`;
   if (boxes <= 6) {
@@ -88,8 +124,6 @@ const displayRoundsFunction = () => {
     displayRounds.style.gridGap = '1px';
   }
 };
-
-displayRoundsFunction();
 
 const init = (totalRounds, roundLength, totalLength) => {
   currentRoundDetails.innerText = `${
@@ -117,18 +151,7 @@ const init = (totalRounds, roundLength, totalLength) => {
   });
 };
 
-let currentRound = roundLength;
-let totalRemaining = totalLength;
-let currentInterval = 1;
-let warmUpPassed = 0;
-
-init(totalRounds, roundLength, totalLength);
-
-let elapsedSeconds = 0,
-  t;
-
 const addSeconds = () => {
-  // TODO - Create a description in the current round
   // If warm up is going display Warm Up and the show the warm up (how many secs left), If the workout is going show the round number and the remaining secs of the current round
   if (warmUpLength > warmUpPassed) {
     document.querySelector('.box-0').classList.add('box--active');
@@ -210,10 +233,23 @@ const resetTimer = () => {
   pauseBtn.classList.add('hidden');
 };
 
+const nextRound = () => {
+  console.log('next');
+};
+
+const prevRound = () => {
+  console.log('prev');
+};
+
+// ---------- Event Listeners ----------
+// Set Workout
+settings.addEventListener('submit', setWorkout);
+
 //  Start button
 playBtn.addEventListener('click', function () {
   playBtn.classList.toggle('hidden');
   pauseBtn.classList.toggle('hidden');
+  // audioUnpause.play();
   timer();
 });
 
@@ -221,8 +257,28 @@ playBtn.addEventListener('click', function () {
 pauseBtn.addEventListener('click', function () {
   playBtn.classList.toggle('hidden');
   pauseBtn.classList.toggle('hidden');
+  // audioPause.play();
   pauseTimer();
 });
 
 //  Stop button
 resetBtn.addEventListener('click', resetTimer);
+
+// Next button
+nextBtn.addEventListener('click', nextRound);
+
+// Prev Button
+prevBtn.addEventListener('click', prevRound);
+
+// Init
+displayRoundsFunction();
+init(totalRounds, roundLength, totalLength);
+
+// TODO - Add .overlay
+// TODO - format settings in CSS
+// TODO - Hide settings
+
+// TODO - Implement next/prev buttons
+
+// IDEA - Implement named workouts, that's probably backend
+// TODO - Create a description in the current round
